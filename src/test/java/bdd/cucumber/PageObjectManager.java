169b1504b57e782a66344761ae45ff7ebf2061f8
task <muhammedkeskin88@gmail.com>
2023-12-03 12:47:58 +0300

    enuygun task

---
+++ src/test/java/bdd/cucumber/PageObjectManager.java
@@ -0,0 +1,8 @@
+package bdd.cucumber;
+
+import bdd.steps.HomePageSteps;
+
+public class PageObjectManager {
+
+    public HomePageSteps homePageSteps = new HomePageSteps();
+}
---
+++ src/test/java/bdd/step_definitions/Hooks.java
@@ -0,0 +1,27 @@
+package bdd.step_definitions;
+
+import bdd.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Driver.getDriver().manage().window().maximize();
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
+}
